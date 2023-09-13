054a812c8608badf77b743da07e2710675f5ad6e
Svprnce <Svprnce@gmail.com>
2023-09-13 22:36:51 +0530

    Fixed Serverless Function on Netlify

--- functions/hellonew/hellonew.js
+++ functions/hellonew/hellonew.js
@@ -1,17 +1,5 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
-  // try {
-  //   const subject = event.queryStringParameters
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({ message: `Hello ${subject.name}` }),
-  //     // // more keys you can return:
-  //     // headers: { "headerName": "headerValue", ... },
-  //     // isBase64Encoded: true,
-  //   }
-  // } catch (error) {
-  //   return { statusCode: 500, body: error.toString() }
-  // }
   try {
     const subject = event.queryStringParameters
     const response = await fetch(`https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${subject.query}`);
